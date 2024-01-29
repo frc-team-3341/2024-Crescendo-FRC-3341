7ed0b63ac201bc57b36c6040ffa0e0c7e326dcbd
Yodaman07 <ayaan.irshad@gmail.com>
2024-01-28 20:51:56 -0800

    Reworked how to receive data for the z angle and x offset. Also added a file to center the tag. I just need help on how to move the robot with swerve (rotating and strafing)

--- 2024-Competition-Code/src/main/java/frc/robot/subsystems/photonvision/PhotonVision.java
+++ 2024-Competition-Code/src/main/java/frc/robot/subsystems/photonvision/PhotonVision.java
@@ -1,15 +1,11 @@
 package frc.robot.subsystems.photonvision;
 
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
@@ -18,11 +14,10 @@ public class PhotonVision extends SubsystemBase {
     public PhotonCamera photonCamera;
     public PhotonPipelineResult result;
     public PhotonTrackedTarget target;
+    public Transform3d targetPos;
     public static boolean hasTarget;
-    public static double yaw;
-    public static double pitch;
-    public static double area;
-    public static double skew;
+    public double ZAngle; //Angle of rotation to correct with the AprilTag
+    public double XOffset; //the X-offset with the AprilTag
 
 
     public PhotonVision() {
@@ -37,56 +32,56 @@ public class PhotonVision extends SubsystemBase {
         return hasTarget;
     }
 
-    public double getYaw(){ return yaw; }
-    public double getPitch(){
-        return pitch;
-    }
-    public double getArea(){
-        return area;
-    }
-    public double getSkew(){
-        return skew;
-    }
+//    public Rotation2d getYawOffset(){
+//        Pose2d robotPose;
+//        Pose2d targetPose;
+//        //getYawToPose provides a delta yaw between the robot and the apriltag
+//        //Need to calculate the robot and apriltag pose
+//        //We want to move the robot until the yaw is 0 or within a threshold near 0
+//        //We are using the built-in yaw for now just to get things working <-- Can change and update later
+//        double yaw = this.getYaw();
+//        return Rotation2d.fromDegrees(yaw);
+//    }
+//
+//    public double getXOffset1(){
+//        double distanceToTargetMeters = PhotonUtils.calculateDistanceToTargetMeters(
+//                Constants.PhotonVisionConstants.cameraHeightMeters,
+//                Constants.PhotonVisionConstants.targetHeightMeters,
+//                Constants.PhotonVisionConstants.cameraPitchRadians,
+//                Constants.PhotonVisionConstants.targetPitchRadians
+//        );
+//
+//        Translation2d dist = PhotonUtils.estimateCameraToTargetTranslation(distanceToTargetMeters, Rotation2d.fromRadians(this.getYaw()));
+//        //Should return the x offset from the target (can use y offset to get the distance which can be adjusted for different distances)
+//        return dist.getX();
+//    }
 
-    public Rotation2d getYawOffset(){
-        Pose2d robotPose;
-        Pose2d targetPose;
-        //getYawToPose provides a delta yaw between the robot and the apriltag
-        //Need to calculate the robot and apriltag pose
-        //We want to move the robot until the yaw is 0 or within a threshold near 0
-        //We are using the built-in yaw for now just to get things working <-- Can change and update later
-        double yaw = this.getYaw();
-        return Rotation2d.fromDegrees(yaw);
+    public double getZAngle(){
+        return targetPos.getRotation().getZ();
     }
 
     public double getXOffset(){
-        double distanceToTargetMeters = PhotonUtils.calculateDistanceToTargetMeters(
-                Constants.PhotonVisionConstants.cameraHeightMeters,
-                Constants.PhotonVisionConstants.targetHeightMeters,
-                Constants.PhotonVisionConstants.cameraPitchRadians,
-                Constants.PhotonVisionConstants.targetPitchRadians
-        );
-
-        Translation2d dist = PhotonUtils.estimateCameraToTargetTranslation(distanceToTargetMeters, Rotation2d.fromRadians(this.getYaw()));
-        //Should return the x offset from the target (can use y offset to get the distance which can be adjusted for different distances)
-        return dist.getX();
+        return targetPos.getY(); //Y is the left direction but the offset is on a 2D x-axis
     }
 
-    //Z Angle can determine if the camera is flat to the april tag <-- Needs calibration first??
+    //Z Angle can determine if the camera is flat to the april tag <-- Needs calibration first
     @Override
     public void periodic() {
         if (this.targetExists()){
             target = result.getBestTarget();
-            yaw = target.getYaw();
-            pitch = target.getPitch();
-            area = target.getArea();
-            skew = target.getSkew();
-            //Skew doesn't work with April-Tags
+            targetPos = target.getBestCameraToTarget();
         }
         SmartDashboard.putBoolean("Target", this.targetExists());
-        SmartDashboard.putNumber("Yaw", yaw);
-        SmartDashboard.putNumber("Pitch", pitch);
-        SmartDashboard.putNumber("Area", area);
-        SmartDashboard.putNumber("Skew", skew);
+
+        //Its confusingly described how the cameraToTarget method works and what it gives, so I have put a bunch of targetPosData values set to print for testing. The correct value will become the "Z angle" which can be plugged into the rest of the program
+        //We also can use this for the y m which gives the x positioning of the april tag which can once again be plugged in when properly tested
+        SmartDashboard.putNumber("targetPosData_1", targetPos.getX());
+        SmartDashboard.putNumber("targetPosData_2", targetPos.getY());
+        SmartDashboard.putNumber("targetPosData_3", targetPos.getZ());
+
+        SmartDashboard.putNumber("targetPosData_4", targetPos.getRotation().getAngle());
+        SmartDashboard.putNumber("targetPosData_5", targetPos.getRotation().getX());
+        SmartDashboard.putNumber("targetPosData_6", targetPos.getRotation().getY());
+        SmartDashboard.putNumber("targetPosData_7", targetPos.getRotation().getZ());
     }
 }
