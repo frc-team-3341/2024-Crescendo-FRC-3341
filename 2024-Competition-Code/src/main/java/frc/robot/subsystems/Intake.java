50ab5152e6bce77fcdb6806837d80ae3235413de
NathanS54 <lenali3500@gmail.com>
2024-02-09 18:26:16 -0800

    Changed the beambreak and the Intake to be in one subsystem.

--- 2024-Competition-Code/src/main/java/frc/robot/subsystems/Intake.java
+++ 2024-Competition-Code/src/main/java/frc/robot/subsystems/Intake.java
@@ -17,27 +17,29 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-boolean timerStarted = false;
- Timer timer = new Timer();
- double seconds;
- DigitalInput beamBreak = new DigitalInput(Constants.BeamBreak.beamID1);
- DigitalInput beamBreak2 = new DigitalInput(Constants.BeamBreak.beamID2);
-   SmartDashboard.putBoolean("beamBreak", getSensor());
-      SmartDashboard.putBoolean("beamBreak2", getSensor());
-   SmartDashboard.putNumber("seconds", seconds);
-   SmartDashboard.putNumber("timer", timer.get());
-    // Declare a Talon SRX motor controller for the flywheel
-    private final WPI_TalonSRX FlyWheelTalon;
-      // Initialize the Talon SRX motor controller for the flywheel with a specific port
-      FlyWheelTalon = new WPI_TalonSRX(Constants.IntakePorts);
+  boolean timerStarted = false;
+  Timer timer = new Timer();
+  double seconds;
+  DigitalInput beamBreak = new DigitalInput(Constants.BeamBreak.beamID1);
+  DigitalInput beamBreak2 = new DigitalInput(Constants.BeamBreak.beamID2);
 
+  // Declare a Talon SRX motor controller for the flywheel
+    // Initialize the Talon SRX motor controller for the flywheel with a specific port
+  private final WPI_TalonSRX FlyWheelTalon = new WPI_TalonSRX(Constants.IntakePorts);
+
+  public Intake(){
       // Set the inversion of the flywheel motor controller (whether it runs in the opposite direction)
-      FlyWheelTalon.setInverted(false);
+  FlyWheelTalon.setInverted(false);
   
-      FlyWheelTalon.configFactoryDefault();
-    }
+  FlyWheelTalon.configFactoryDefault();
+  }
+
+
  public boolean getSensor(){
-   return !beamBreak.get();
+  return !beamBreak.get();
+ }
+
+  public boolean getSensor2(){
    return !beamBreak2.get();
  }
 
@@ -52,27 +54,32 @@ boolean timerStarted = false;
      timer.reset();
      timer.start();
      timerStarted = true; 
-   }
-         @Override
-    public void simulationPeriodic() {
-      // Simulation-specific code
-    }
-   }
-  
-   if(!getSensor() && timerStarted){
+
+      if(!getSensor() && timerStarted){
      timer.stop();
      timerStarted = false;
-   }
+    }
+    
 
 
-   if(getSecondSensor()){
+   if(getSensor2()){
     if(timer.get() > 0){
        seconds = timer.get();
       }
       timer.stop();
       timer.reset();
-    //
-    //
+   }
+  }
+
+  SmartDashboard.putBoolean("beamBreak", getSensor());
+  SmartDashboard.putBoolean("beamBreak2", getSensor2());
+  SmartDashboard.putNumber("seconds", seconds);
+  SmartDashboard.putNumber("timer", timer.get());
+}
+         @Override
+    public void simulationPeriodic() {
+      // Simulation-specific code
+    }
   
     // Method to set the power of the flywheel motor controller
    
