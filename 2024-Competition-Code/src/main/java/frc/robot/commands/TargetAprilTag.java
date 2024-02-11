3d2b237bd0bf9d3c90b825c8f69993ef69a1504a
Yodaman07 <ayaan.irshad@gmail.com>
2024-02-10 16:16:11 -0800

    Renamed the photonvision package

--- 2024-Competition-Code/src/main/java/frc/robot/commands/TargetAprilTag.java
+++ 2024-Competition-Code/src/main/java/frc/robot/commands/TargetAprilTag.java
@@ -2,7 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.PhotonVision.PhotonVision;
+import frc.robot.subsystems.photonvision.PhotonVision;
 
 
 public class TargetAprilTag extends Command {
