7460e515b6f3b91ad0acc0b084413ec4c49e17bb
Aric Volman <89823522+aric-volman@users.noreply.github.com>
2024-03-02 15:04:48 -0800

    Update IntakeManual.java from Rayyan's testing

--- 2024-Competition-Code/src/main/java/frc/robot/commands/notemechanism/IntakeManual.java
+++ 2024-Competition-Code/src/main/java/frc/robot/commands/notemechanism/IntakeManual.java
@@ -2,8 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.notemechanism;
+package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter;
 
@@ -25,13 +26,13 @@ public class IntakeManual extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    shooter.setFeedSimple(power);
+    shooter.setIntakePower(power);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    shooter.setFeedSimple(0);
+    shooter.setIntakePower(0);
   }
 
   // Returns true when the command should end.
